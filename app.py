cd1e72924bfeb6353e28c874664fff375a70486f
Patrick J. Burns <patrick@diyclassics.org>
2018-01-30 11:13:34 -0500

    Add pyzotero; read library

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import os
-from flask import Flask
-
+from flask import Flask, render_template
+from pyzotero import zotero
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+library_id = os.getenv('LIBRARY_ID')
+library_type = os.getenv('LIBRARY_TYPE')
+api_key = os.getenv('API_KEY')
+
+z = zotero.Zotero(library_id, library_type, api_key)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+def homepage():
+    isawbib = z.everything(z.top())
+    count = len(isawbib)
+    #tags = get_tags(isawbib)
+    return render_template('isaw-bib.html', isawbib=isawbib, count=count)
 
 if __name__ == '__main__':
     app.run()
