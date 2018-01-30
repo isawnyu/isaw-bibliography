24daf8b7b39191b13e948277ae13447d624608e1
Patrick J. Burns <patrick@diyclassics.org>
2018-01-30 12:08:14 -0500

    Update app to pull in html citations

--- app.py
+++ app.py
@@ -12,14 +12,23 @@ library_type = os.getenv('LIBRARY_TYPE')
 api_key = os.getenv('API_KEY')
 
 z = zotero.Zotero(library_id, library_type, api_key)
+isawbib_json = z.everything(z.top())
+cit = z.add_parameters(content='bib', style='mla')
+isawbib_cit = z.everything(z.top())
 
 @app.route('/')
 def homepage():
-    isawbib = z.everything(z.top())
-    count = len(isawbib)
+    count = len(isawbib_json)
     #tags = get_tags(isawbib)
-    pprint(isawbib[0]['links'])
-    return render_template('isaw-bibliography.html', isawbib=isawbib, count=count)
+    return render_template('isaw-bibliography.html', isawbib=isawbib_json, count=count)
+
+
+@app.route('/bib')
+def bib():
+    count = len(isawbib_cit)
+    #tags = get_tags(isawbib)
+    return render_template('isaw-citations.html', isawbib=isawbib_cit, count=count)
+
 
 if __name__ == '__main__':
     app.run()
